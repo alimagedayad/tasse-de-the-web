9a56f2877edecf4af4be4609ae725e9401d03ddd
Ali M. Ayad <alimagedayad@users.noreply.github.com>
2021-04-25 20:30:23 +0200

    chore(root): new keys
    Took 23 minutes

--- db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "remotemysql.com",
-    USER: "gW25cgxZdA",
-    PASSWORD: "wEaTbOfQSj",
-    DB: "gW25cgxZdA"
-};
--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-const mysql = require("mysql");
-const dbConfig = require("./db.config.js");
+require("dotenv").config();
 
 // Create a connection to the database
 const connection = {
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DB,
     multipleStatements: true
 };
 
